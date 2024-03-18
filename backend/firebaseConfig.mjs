a3b9756f39efd98bd988178a3e417d55e464acd7
BHAVYAPODDAR <117264275+BHAVYAPODDAR@users.noreply.github.com>
2024-03-18 23:57:14 +0530

    Update firebaseConfig.mjs
    Removed api key for firebase

--- backend/firebaseConfig.mjs
+++ backend/firebaseConfig.mjs
@@ -7,13 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCO2P5DJBhFOqmD3ba5QzyIuclGf24ypK8",
-  authDomain: "synergy-35091.firebaseapp.com",
-  projectId: "synergy-35091",
-  storageBucket: "synergy-35091.appspot.com",
-  messagingSenderId: "120925986035",
-  appId: "1:120925986035:web:04ced91bbeebdd0f9ceb86",
-  measurementId: "G-HY022KRC96",
+  
 };
 
 // Initialize Firebase
